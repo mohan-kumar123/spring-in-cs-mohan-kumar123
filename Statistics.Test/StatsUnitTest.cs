2f5be3bc9284bcec81e2712cf2db18e0d76759f4
QOH2KOR <QOH2KOR@BMHE1115826>
2022-10-12 23:10:07 +0530

    Commit: Added first set of changes.

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using Statistics;
+using System.Collections.Generic;
 
 namespace Statistics.Test
 {
@@ -11,7 +12,7 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{1.5, 8.9, 3.2, 4.5});
+                new List<double>{1.5, 8.9, 3.2, 4.5});
             float epsilon = 0.001F;
             Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
             Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
@@ -22,21 +23,25 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
+                new List<double>{});
             //All fields of computedStats (average, max, min) must be
             //Double.NaN (not-a-number), as described in
             //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
+            Assert.True(statsComputer.average == 0);
+            Assert.True(statsComputer.average == 0);
+            Assert.True(statsComputer.average == 0);
+
         }
         [Fact]
         public void RaisesAlertsIfMaxIsMoreThanThreshold()
         {
             var emailAlert = new EmailAlert();
             var ledAlert = new LEDAlert();
-            IAlerter[] alerters = {emailAlert, ledAlert};
+            IAlerter[] alerters = { emailAlert, ledAlert };
 
-            const float maxThreshold = 10.2;
+            const double maxThreshold = 10.2;
             var statsAlerter = new StatsAlerter(maxThreshold, alerters);
-            statsAlerter.checkAndAlert(new List<___>{0.2, 11.9, 4.3, 8.5});
+            statsAlerter.checkAndAlert(new List<double> { 0.2, 11.9, 4.3, 8.5 });
 
             Assert.True(emailAlert.emailSent);
             Assert.True(ledAlert.ledGlows);
--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
+        public double average;
+        public double max;
+        public double min;
+
+        public Stats CalculateStatistics(List<double> numbers) {
             //Implement statistics here
+
+            average = Stats.CalculateAverage(numbers);
+            min = Stats.CalculateMin(numbers);
+            max = Stats.CalculateMax(numbers);
+
+            return new Stats();
         }
+    }
+
+    public class Stats
+    {
+        public int _average { get; set; }
+        public int _max { get; set; }
+        public int _min { get; set; }
+
+        private static bool IsNanNumber(List<double> numbers)
+        {
+            Boolean result = false;
+
+            if (numbers.Count > 0)
+            {
+                numbers.ForEach(x => result = double.IsNaN(x)); 
+            }
+            else
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public static double CalculateAverage(List<double> numbers)
+        {
+            double average = 0;
+           if(!IsNanNumber(numbers))
+            {
+                average = numbers.Average();
+                //double sam = Math.Abs(average - 4.525);
+            }
+            
+            return average;
+        }
+        public static double CalculateMax(List<double> numbers)
+        {
+            double average = 0;
+            if (!IsNanNumber(numbers))
+            {
+                average = numbers.Max();
+                //double sam = Math.Abs(average - 4.525);
+            }
+
+            return average;
+        }
+
+        public static double CalculateMin(List<double> numbers)
+        {
+            double average = 0;
+            if (!IsNanNumber(numbers))
+            {
+                average = numbers.Min();
+                //double sam = Math.Abs(average - 4.525);
+            }
+
+            return average;
+        }
+    }
+
+    public class StatsAlerter
+    {
+        private double _maxThreshold;
+        readonly IAlerter[] _Ialerts;
+        public StatsAlerter(double maxThreshold, IAlerter[] alerters)
+        {
+            _maxThreshold = maxThreshold;
+            _Ialerts = alerters;
+        }
+
+        public void checkAndAlert(List<double> inputData)
+        {
+            EmailAlert emailAlert = (EmailAlert)_Ialerts[0];
+            LEDAlert lEDAlert = (LEDAlert)_Ialerts[1];
+
+            var maxData = inputData.Max();
+
+            if(maxData > _maxThreshold)
+            {
+                emailAlert.emailSent = true;
+                lEDAlert.ledGlows = true;
+            }
+        }
+
+    }
+
+    public interface IAlerter
+    {
+      
+    }
+
+    public class EmailAlert: IAlerter
+    {
+       public bool emailSent { get; set; }
+        
+    }
+
+    public class LEDAlert: IAlerter
+    {
+        public bool ledGlows { get; set; }
+        
     }
 }
